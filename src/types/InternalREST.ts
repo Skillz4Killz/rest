5e732baab78260c9920dccbc62ae200b759e2136
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2020-06-01 16:11:47 -0400

    Update InternalREST.ts

--- src/types/InternalREST.ts
+++ src/types/InternalREST.ts
@@ -9,7 +9,7 @@ export type ImageExtension = 'png' | 'gif' | 'webp' | 'jpg' | 'jpeg';
 export type EmojiExtension = 'png' | 'gif';
 
 export interface ImageURLOptions {
-	extension?: string;
-	size?: number;
+	extension?: ImageExtension;
+	size?: 16 | 32 | 64 | 128 | 256 | 512 | 1024 | 2048 | 4096;
 	dynamic?: boolean;
 }
